7c173af9fc1e7dfb23c5d751520636318389d6bb
yshang616 <yshang@macalester.edu>
2022-03-30 16:18:34 +0800

    added search post

--- controllers/posts.js
+++ controllers/posts.js
@@ -1,10 +1,29 @@
+import express from "express";
 import mongoose from "mongoose";
 import PostMessage from "../models/postMessage.js";
 
+const router = express.Router();
+
 export const getPosts = async (req, res)=>{
+
     try{
         const postMessages = await PostMessage.find();
         res.status(200).json(postMessages)
+
+    } catch(error){
+        res.status(404).json({ message:error.message });
+
+    }
+    console.log('THIS WORKS!');
+}
+
+export const getPostsBySearch = async (req, res)=>{
+    const { searchQuery, tags } = req.query;
+
+    try{
+        const title = new RegExp(searchQuery, 'i');
+        const posts = await PostMessage.find({ $or: [{ title }, { tags: { $in: tags.split(',') }}] });
+        res.json({ data: posts });
 
     } catch(error){
         res.status(404).json({ message:error.message });
